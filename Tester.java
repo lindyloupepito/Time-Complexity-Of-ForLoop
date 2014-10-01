fc43ee454f53ebe18a5fda64088f1e46ab400947
Lindy Lou Pepito <lindyloupepito@gmail.com>
2014-10-01 13:01:40 +0800

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmsc142_mp2;
+
+/**
+ *
+ * @author lindyloupepito
+ */
+public class Tester {
+  public static void main(String[] args) {
+    LoopProcessor reader = new LoopProcessor("input.in");
+    reader.extractTokens();
+  }
+}
